ed4bc43f3a898e755e27c67dc9460fbe2f6d5d40
Sonia Peikert <speikert@etisoft.com.pl>
2024-09-05 10:06:19 +0200

    Funkcja CzyPalindron

---
+++ palindron.c
@@ -0,0 +1,9 @@
+/*Sprawdzenie czy podany tekst jest palindronem*/
+#include <stdio.h> /*Operacje wejścia i wyjścia*/
+#include <string.h> /*Operacje na znakach*/
+#define MAX /*stała, maksymalny rozmiar tablicy*/
+int CzyPalindron(char* slowo) /*funkcja zwracająca wartości 1 i 0, char* oznacza ciąg znaków*/
+{
+    int l = strlen(slowo); /*strlen zwraca długość słowa*/
+}
+
