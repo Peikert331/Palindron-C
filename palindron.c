0ed7e25b411f47cbc8753c80f408d93e20ccba46
Sonia Peikert <speikert@etisoft.com.pl>
2024-09-05 11:37:25 +0200

    Funkcja CzyPalindron skończona

--- palindron.c
+++ palindron.c
@@ -4,6 +4,15 @@
 #define MAX /*stała, maksymalny rozmiar tablicy*/
 int CzyPalindron(char* slowo) /*funkcja zwracająca wartości 1 i 0, char* oznacza ciąg znaków*/
 {
-    int l = strlen(slowo); /*strlen zwraca długość słowa*/
+    int dlugosc = strlen(slowo); /*strlen zwraca długość słowa*/
+    int polowa = ceil(dlugosc / 2); /*ceil zaokrągla wynik do góry*/
+    for(int i = 0; i < polowa; i++)
+    {
+        if(slowo[i] =! slowo[dlugosc - 1 - i])
+        {
+            return 0;
+        }
+        return 1;
+    }
 }
 
